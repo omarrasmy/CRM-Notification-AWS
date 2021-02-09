ef6704d2d60c49de05f2a789f500f668448772d4
omarrasmy <omarrasmy00@gmail.com>
2021-02-09 13:28:11 +0200

    first commit

--- Express.js
+++ Express.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: './configurations/dev.env' })
 const path =require('path')
 const AWSUpload = require('./Routes/awsrequest')
 const Notification =  require('./Routes/notification');
-
+console.log("test")
 
 const app = express()
 app.use((req, res, next) => {
